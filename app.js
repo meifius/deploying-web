e036f2dec9d1d18a6d7bcc3d7aec5ba7de7b92ad
Alan Mejia <mejia.alan.r@gmail.com>
2019-03-24 22:14:36 +0000

    update the server

--- app.js
+++ app.js
@@ -2,10 +2,17 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine","ejs");
+
 //ROUTES
 app.get("/", function(req,res){
-    res.send("Hiiiiiiiiiiiii !!!!");
+    res.render("home");
 });
+
+app.get("/about", function(req,res){
+    res.render("about");
+});
+
 
 //Listing
 app.listen(process.env.PORT, process.env.IP, function(req,res){
